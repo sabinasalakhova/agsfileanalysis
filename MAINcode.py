f5cd0002ddf9d120fa25fbc6ae60e2eeed08aa31
sabinasalakhova <sabina.salakhova@aurecongroup.com>
2026-01-16 09:54:16 +0800

    Revert "Last commit message"

--- MAINcode.py
+++ MAINcode.py
@@ -1,13 +1,1 @@
-# Updated MAINcode.py
-
-# This is a sample content of MAINcode.py with updates for Streamlit configurations.
-
-# Replace all occurrences of `use_container_width` with `width`
-
-# Example code that would contain the changes made:
-
-st.write("Content displayed with specified width")  
-# Previous usage
-# st.container(use_container_width=True)  
-# Updated usage
-st.container(width=True)  
+(remainder of MAINcode.py file content here)

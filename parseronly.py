3cfa59f434e8ca77b5f7fc49bf9fddd577c5d1ef
sabinasalakhova <sabina.salakhova@aurecongroup.com>
2026-01-26 11:52:19 +0800

    Add find_hole_id_column import from agsparser

--- parseronly.py
+++ parseronly.py
@@ -12,7 +12,7 @@ import plotly.express as px
 import re
 
 # External modules
-from agsparser import analyze_ags_content, _split_quoted_csv, parse_ags_file
+from agsparser import analyze_ags_content, _split_quoted_csv, parse_ags_file,find_hole_id_column
 from cleaners import deduplicate_cell, drop_singleton_rows, expand_rows, combine_groups, coalesce_columns, to_numeric_safe, normalize_columns
 from triaxial import generate_triaxial_table, generate_triaxial_with_lithology, calculate_s_t_values, remove_duplicate_tests
 

daea5dca81a91952c645f3b9adcfd87eea49be5d
sabinasalakhova <sabina.salakhova@aurecongroup.com>
2026-01-26 10:05:21 +0800

    Add file_bytes assignment before parsing AGS file

--- MAINcode.py
+++ MAINcode.py
@@ -65,6 +65,7 @@ if uploaded_files:
         diagnostics.append((f.name, flags))
 
         # 2) Parse into per-group DataFrames
+        file_bytes = f.getvalue()
         raw_groups: Dict[str, pd.DataFrame] = parse_ags_file(file_bytes, f.name)
         cleaned_groups: Dict[str, pd.DataFrame] = {}
 
